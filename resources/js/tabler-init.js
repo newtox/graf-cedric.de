621209eaaba09593e9a3cb40795ec0d7c7a41a24
newtox <futzy261@gmail.com>
2024-12-07 22:23:57 +0100

    Add: Frontend assets

---
+++ resources/js/tabler-init.js
@@ -0,0 +1,7 @@
+import jQuery from "jquery";
+
+window.$ = window.jQuery = jQuery;
+
+
+import '../../node_modules/@tabler/core/dist/js/tabler';
+import '../../node_modules/@tabler/core/src/js/demo-theme';
