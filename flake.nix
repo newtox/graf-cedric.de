221efff618eaa93e77ae45d2cc75d611e54ed727
newtox <futzy261@gmail.com>
2026-08-04 01:04:11 +0200

    chore: add nix flake configuration

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "graf-cedric.de personal website";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            php83
+            php83Packages.composer
+            nodejs_22
+          ];
+        };
+      }
+    );
+}
